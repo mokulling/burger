3190973756a29ef9e6f18698cb478f335f9454a3
mokulling <72229978+mokulling@users.noreply.github.com>
2021-02-13 19:25:39 -0600

    set up orm

--- models/burger.js
+++ models/burger.js
@@ -2,6 +2,23 @@ const orm = require('../config/orm')
 
 
 //Create code that will call the ORM functions using burger specific input for the ORM 
+const burger = {
+    all: function(cb){
+        orm.all('burgers', function(res){
+            cb(res);
+        });
+
+    },
+    create: function(cols, vals, cb){
+
+    },
+    update: function(objColVals, condition, cb) {
+        
+    }
 
 
-module.exports = burger
+}
+
+
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-const express = require('express')
+const mysql = require('mysql')
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const PORT = process.env.PORT || 3000
+const connection = require('./config/connection')
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+const routes = require('./controllers/burgers_controller')
+
+app.use(routes)
+
+app.listen(PORT, function () {
+    console.log(`listening on localhost:${PORT}`)
+})
