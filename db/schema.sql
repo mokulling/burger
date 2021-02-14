f2d97920edf65334d734c4ca437984057175a358
mokulling <72229978+mokulling@users.noreply.github.com>
2021-02-13 19:09:51 -0600

    Set up handlebars and express router

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR(30),
+    devoured BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (id) 
+
+
+)
+
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veg', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Deluxe', DEFAULT)
+
+
